9b4d3e58665207c59d0e00427b09ace2df468f91
mikedeweyvogt@gmail.com <mikedeweyvogt@gmail.com@e89e8a17-12d6-65e8-a1ae-4aa94e32fae3>
2011-10-11 07:18:12 +0000

    added new commands

---
+++ trunk/Exeggcute/src/commands/ShootCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exeggcute.src.commands
+{
+    class ShootCommand : Command
+    {
+        public int ShotIndex { get; protected set; }
+
+        public ShootCommand(int shotIndex)
+            : base(CommandType.Shoot)
+        {
+            ShotIndex = shotIndex;
+        }
+
+    }
+}
