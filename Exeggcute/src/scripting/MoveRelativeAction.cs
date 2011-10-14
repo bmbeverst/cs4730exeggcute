0dbf3c446ed0d78c531fa4439d082c1ba8460fb2
mikedeweyvogt <mikedeweyvogt@gmail.com>
2011-10-14 18:36:30 +0000

    Changed how move commands work

---
+++ Exeggcute/src/scripting/MoveRelativeAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Exeggcute.src.entities;
+
+namespace Exeggcute.src.scripting
+{
+    class MoveRelativeAction : ActionBase
+    {
+        public Vector3 Displacement { get; protected set; }
+        public int Duration { get; protected set; }
+
+
+        public MoveRelativeAction(Vector3 displacement, int duration)
+        {
+            Displacement = displacement;
+            Duration = duration;
+        }
+
+        public override void Process(CommandEntity entity)
+        {
+            entity.Process(this);
+        }
+
+        public override ActionBase Copy()
+        {
+            return new MoveRelativeAction(Displacement, Duration);
+        }
+    }
+}
