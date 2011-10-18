23184dfb8180083d69ce4574e787e79ec9e00053
mikedeweyvogt@gmail.com <mikedeweyvogt@gmail.com@e89e8a17-12d6-65e8-a1ae-4aa94e32fae3>
2011-10-18 08:09:56 +0000

    More refactoring

--- trunk/Exeggcute/src/graphics/Quad.cs
+++ trunk/Exeggcute/src/graphics/Quad.cs
@@ -9,76 +9,80 @@ namespace Exeggcute.src.graphics
 {
     class Quad
     {
-        /*
-         * Vertices[0].Position = BottomLeft;
-            Vertices[0].TextureCoordinate = textureBottomLeft;
-
-            Vertices[1].Position = TopLeft;
-            Vertices[1].TextureCoordinate = textureTopLeft;
-
-            Vertices[2].Position = BottomRight;
-            Vertices[2].TextureCoordinate = textureBottomRight;
-
-            Vertices[3].Position = TopRight;
-            Vertices[3].TextureCoordinate = textureTopRight;
-         */
         public Vector3 Center;
         public Vector3 BottomLeft
         {
             get { return Vertices[0].Position; }
-            set { Vertices[0].Position = value; }
+            protected set { Vertices[0].Position = value; }
         }
         public Vector3 TopLeft
         {
             get { return Vertices[1].Position; }
-            set { Vertices[1].Position = value; }
+            protected set { Vertices[1].Position = value; }
         }
         public Vector3 BottomRight
         {
             get { return Vertices[2].Position; }
-            set { Vertices[2].Position = value; }
+            protected set { Vertices[2].Position = value; }
         }
         public Vector3 TopRight
         {
             get { return Vertices[3].Position; }
-            set { Vertices[3].Position = value; }
+            protected set { Vertices[3].Position = value; }
         }
-        
-        public Vector3 Normal;
-        public Vector3 Up;
-        public Vector3 Left;
 
         public Vector2 UVBottomLeft
         {
             get { return Vertices[0].TextureCoordinate;  }
-            set { Vertices[0].TextureCoordinate = value; }
+            protected set { Vertices[0].TextureCoordinate = value; }
         }
 
         public Vector2 UVTopLeft
         {
             get { return Vertices[1].TextureCoordinate; }
-            set { Vertices[1].TextureCoordinate = value; }
+            protected set { Vertices[1].TextureCoordinate = value; }
         }
 
         public Vector2 UVBottomRight
         {
             get { return Vertices[2].TextureCoordinate; }
-            set { Vertices[2].TextureCoordinate = value; }
+            protected set { Vertices[2].TextureCoordinate = value; }
         }
 
         public Vector2 UVTopRight
         { 
             get { return Vertices[3].TextureCoordinate;  }
-            set { Vertices[3].TextureCoordinate = value; }
+            protected set { Vertices[3].TextureCoordinate = value; }
         }
         
        
 
         public VertexPositionNormalTexture[] Vertices;
         public short[] Indexes;
+
         public float Width { get; protected set; }
         public float Height { get; protected set; }
 
+        /// <summary>
+        /// A texture mapped quad.
+        /// </summary>
+        /// 
+        /// <param name="topleft">
+        /// Vector in texel space (range 0 to 1) pointing to the upper left 
+        /// portion of the texture we wish to map onto this quad
+        /// </param>
+        /// 
+        /// <param name="bottomRight">
+        /// Vector in texel space (range 0 to 1) pointing to the bottom right
+        /// portion of the texture we wish to map onto this quad
+        /// </param>
+        /// 
+        /// <param name="position">T
+        /// he position of the center of this quad.
+        /// </param>
+        /// <param name="back">Vector pointing towards the camera.</param>
+        /// <param name="width">width of the quad</param>
+        /// <param name="height">height of the quad</param>
         public Quad(Vector2 topleft,
             Vector2 bottomRight,
             Vector3 position,
@@ -96,19 +100,20 @@ namespace Exeggcute.src.graphics
             Vertices = new VertexPositionNormalTexture[4];
             Indexes = new short[6]; ;
             Center = center;
-            Normal = back;
-            Up = Vector3.Up;
+            Vector3 normal = back;
+            Vector3 up = Vector3.Up;
             UVTopLeft = texTopLeft;
             UVTopRight = new Vector2(texBottomRight.X, texTopLeft.Y);
             UVBottomLeft = new Vector2(texTopLeft.X, texBottomRight.Y);
             UVBottomRight = texBottomRight;
-            // Calculate the quad corners
-            Left = Vector3.Cross(Normal, Up);
-            Vector3 topcenter = (Up * Height / 2) + Center;
-            TopLeft = topcenter + (Left * Width / 2);
-            TopRight = topcenter - (Left * Width / 2);
-            BottomLeft = TopLeft - (Up * Height);
-            BottomRight = TopRight - (Up * Height);
+            
+            // I know this seems backwards, but it works
+            Vector3 left = Vector3.Cross(normal, up);
+            Vector3 topcenter = Center + (up * Height / 2) ;
+            TopLeft = topcenter + (left * Width / 2);
+            TopRight = topcenter - (left * Width / 2);
+            BottomLeft = TopLeft - (up * Height);
+            BottomRight = TopRight - (up * Height);
             recalculateNormals();
 
             Indexes[0] = 0;
@@ -134,11 +139,11 @@ namespace Exeggcute.src.graphics
         {
             Vector3 right = TopLeft - TopRight;
             Vector3 down = TopRight - BottomRight;
-            Normal = Vector3.Cross(down, right);
-            Normal.Normalize();
+            Vector3 normal = Vector3.Cross(down, right);
+            normal.Normalize();
             for (int i = 0; i < Vertices.Length; i++)
             {
-                Vertices[i].Normal = Normal;
+                Vertices[i].Normal = normal;
             }
         }
 
