d0ad02b33547423f030b36342ef14423225d2d0c
mikedeweyvogt@gmail.com <mikedeweyvogt@gmail.com@e89e8a17-12d6-65e8-a1ae-4aa94e32fae3>
2011-10-19 20:20:44 +0000

    Fixed things

---
+++ trunk/Exeggcute/src/entities/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exeggcute.src.assets;
+using Microsoft.Xna.Framework;
+
+namespace Exeggcute.src.entities
+{
+    class Mover : CommandEntity
+    {
+        public Mover(ScriptName moveScript)
+            : base(moveScript)
+        {
+
+        }
+
+        public void Update(Vector3 parentPos, float parentAngle)
+        {
+            Position = parentPos;
+            Angle = parentAngle;
+            base.Update();
+        }
+    }
+}
---
+++ trunk/Exeggcute/src/entities/ParentEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exeggcute.src.scripting.arsenal;
+using Exeggcute.src.assets;
+using Exeggcute.src.scripting.action;
+
+namespace Exeggcute.src.entities
+{
+    /// <summary>
+    /// A parent entity contains an arsenal.
+    /// </summary>
+    class ParentEntity : CommandEntity
+    {
+        protected NewArsenal arsenal;
+        protected HashList<Gib> gibList;
+        protected HashList<Shot> shotList;
+
+        public ParentEntity(ModelName modelName,
+            ScriptName scriptName,
+            ArsenalName arsenalName,
+            HashList<Shot> shotListHandle,
+            HashList<Gib> gibListHandle)
+            : base(modelName, scriptName)
+        {
+            List<ArsenalEntry> entries = ArsenalBank.Get(arsenalName);
+            this.gibList = gibListHandle;
+            this.shotList = shotListHandle;
+            Console.WriteLine(shotListHandle.Name);
+            this.arsenal = new NewArsenal(entries, shotListHandle);
+        }
+
+        public override void Process(ShootAction shoot)
+        {
+            arsenal.Process(shoot);
+            actionPtr += 1;
+        }
+
+        public override void Update()
+        {
+ 	        base.Update();
+
+            arsenal.Update(Position, Angle);
+        }
+    }
+}
