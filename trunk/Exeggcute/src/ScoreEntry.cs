65961c3c4eac69165fb93399194577edbeccf2cd
mikedeweyvogt@gmail.com <mikedeweyvogt@gmail.com@e89e8a17-12d6-65e8-a1ae-4aa94e32fae3>
2011-10-26 20:53:42 +0000

    changed score format

--- trunk/Exeggcute/src/ScoreEntry.cs
+++ trunk/Exeggcute/src/ScoreEntry.cs
@@ -9,26 +9,35 @@ namespace Exeggcute.src
 {
     class ScoreEntry
     {
-        public int Score { get; protected set; }
+        public string StringScore { get; protected set; }
+        public int IntScore { get; protected set; }
         public string Name { get; protected set; }
         public DateTime Time { get; protected set; }
         public ScoreEntry(int score, string name, DateTime time)
         {
-            Score = score;
-            Name = name;
-            Time = time;
+
+            this.StringScore = IntToString(score);
+            this.IntScore = score;
+            this.Name = name;
+            this.Time = time;
         }
 
         public ScoreEntry()
         {
-            Score = default(int);
-            Name = "AAA";
-            Time = default(DateTime);
+            this.IntScore = 0;
+            this.StringScore = IntToString(IntScore);
+            this.Name = "AAA";
+            this.Time = default(DateTime);
+        }
+
+        public static string IntToString(int x)
+        {
+            return string.Format("{0:000,000,000}", x);
         }
 
         public override string ToString()
         {
-            return string.Format("{0},{1},{2}", Score, Name, Time);
+            return string.Format("{0},{1},{2}", StringScore, Name, Time);
         }
 
         public void Draw(SpriteBatch batch, SpriteFont font, Vector2 pos, Color color)
